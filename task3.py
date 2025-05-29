64c84ccb936a4da44130fc925351ec1d78f58b9d
Madelavishnu <madelavishnu40@gmail.com>
2025-05-29 19:23:57 +0530

    updated task3.py

--- task3.py
+++ task3.py
@@ -1,3 +1,6 @@
+# housing dataset price prediction using LinearRegression
+
+
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
@@ -7,9 +10,9 @@ from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('/Housing.csv')
+df = pd.read_csv('/Housing.csv')  #upload housing price dataset
 df
-
+#encode the data
 le = LabelEncoder()
 
 #yes = 1   no = 0
@@ -20,7 +23,7 @@ df['mainroad'] = le.fit_transform(df['mainroad'])
 df['airconditioning'] = le.fit_transform(df['airconditioning'])
 df['prefarea'] = le.fit_transform(df['prefarea'])# c =0 q =1 s =2 
 df
-
+#check is there and null values are there
 print(df.isnull().sum())
 print(df[df.isnull().any(axis=1)])
 df.drop(['furnishingstatus'],axis = 1, inplace = True)
@@ -32,13 +35,15 @@ scaler = StandardScaler()
 df[num_coloumn] = scaler.fit_transform(df[num_coloumn])
 df.head()
 
-
 x = df.drop(['price'],axis = 1)
 y = df['price']
 
+
+#split the data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 lr = LinearRegression()
+lr.fit(x_train, y_train)
 y_pred = lr.predict(x_test)
 
 mae = mean_absolute_error(y_test, y_pred)
@@ -48,6 +53,8 @@ r2 = r2_score(y_test, y_pred)
 print(f"MAE: {mae:.2f}")
 print(f"MSE: {mse:.2f}")
 print(f"R² Score: {r2:.4f}")
+
+#plot the graph
 
 plt.figure(figsize=(8, 6))
 sns.scatterplot(x=y_test, y=y_pred, alpha=0.7, color='blue')
